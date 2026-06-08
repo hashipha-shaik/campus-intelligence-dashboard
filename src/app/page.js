ac12d62380a38f6e61197d5c7dbf095b43954141
hashipha-shaik <hashiphashaik@gmail.com>
2026-06-08 12:27:08 +0530

    Day 1 setup complete

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+// import Image from "next/image";
+
+ export default function Home() {
+  return (
+    <main>
+      <h1>Campus Intelligence Dashboard</h1>
+      <p>My MARS Open Project 2026</p>
+    </main>
+  );
+}
