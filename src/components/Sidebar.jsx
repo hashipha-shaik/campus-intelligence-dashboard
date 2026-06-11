281f86841ea6cbd2a2a2227a3b2e5fcddb48e080
hashipha-shaik <hashiphashaik@gmail.com>
2026-06-11 16:12:22 +0530

    Added sidebar navigation

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+function Sidebar() {
+  return (
+    <div
+      style={{
+        width: "220px",
+        backgroundColor: "#1e293b",
+        color: "white",
+        minHeight: "100vh",
+        padding: "20px",
+      }}
+    >
+      <h2>Menu</h2>
+
+      <p>🏠 Dashboard</p>
+      <p>📚 Library</p>
+      <p>📅 Events</p>
+      <p>🎓 Academics</p>
+      <p>💼 Placements</p>
+      <p>🍽 Cafeteria</p>
+      <p>🤖 AI Assistant</p>
+    </div>
+  );
+}
+
+export default Sidebar;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,13 @@
 import Card from "../components/Card";
 import sections from "../data/sections";
-
+import Sidebar from "../components/Sidebar";
 function Dashboard() {
-  return (
-    <div>
+  
+     return (
+  <div style={{ display: "flex" }}>
+    <Sidebar />
+
+    <div style={{ flex: 1, padding: "20px" }}>
       <h2>Campus Services</h2>
 
       <div className="grid">
@@ -16,7 +20,9 @@ function Dashboard() {
         ))}
       </div>
     </div>
-  );
+  </div>
+);
+   
 }
 
 export default Dashboard;
